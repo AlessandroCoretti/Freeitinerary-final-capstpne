ce047fb4db75d47218580c56533d07f5e5161196
Alessandro Coretti <alessandrocoretti2002@gmail.com>
2025-04-27 11:17:31 +0200

    commits3 - correct

--- backend/src/main/java/com/travelfree/freeitinerary/config/SecurityConfig.java
+++ backend/src/main/java/com/travelfree/freeitinerary/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "https://freeitinerary-final-capstpne-m1nmuqm7z.vercel.app",
+                "https://main-rebecca-alessandrocoretti-99c3ef12.koyeb.app",
                 "http://localhost:5173"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
